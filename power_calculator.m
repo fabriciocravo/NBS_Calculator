bf080eee6496479352cbffe8a8a9b59b73c13e8a
Cravo Gomes <f.cravogomes@FACK49Y42CYYY.tld>
2024-11-06 08:32:47 -0500

    Added data reader and subject id extraction

--- power_calculator.m
+++ power_calculator.m
@@ -2,12 +2,23 @@
     % Power Calculator
 %%%%%
 
-clear;
+a = 10;
+vars = who;       % Get a list of all variable names in the workspace
+vars(strcmp(vars, 'data_matrix')) = [];  % Remove the variable you want to keep from the list
+clear(vars{:});   % Clear all other variables
 clc;
+
+if ~exist('data_matrix', 'var')
+    data_matrix = load('./data/s_hcp_fc_noble_tasks.mat');
+else
+    disp('Data already loaded')
+end
 
 [current_path,~,~]=fileparts(mfilename('fullpath')); % assuming NBS_benchmarking is current folder
 addpath(genpath(current_path));
 
 Params = setparams();
+Params.data = data_matrix;
+
 RepParams = setup_benchmarking(Params, false);
 
