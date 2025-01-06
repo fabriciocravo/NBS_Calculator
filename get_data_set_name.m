b01e06e16f0f1d6f8257f67dabf32520ae9c30fb
Fabricio Cravo <fabricio.cravo1@outlook.com>
2025-01-06 13:53:27 -0500

    Fixed bugs for discovery

--- get_data_set_name.m
+++
@@ -1,4 +0,0 @@
-function data_set_name = get_data_set_name(Dataset)
-
-    data_set_name = strcat(Dataset.study_info.dataset, '_', Dataset.study_info.map);
-end
--- test.m
+++
@@ -1,9 +0,0 @@
-function NewParams = test(Params)
-
-    NewParams = Params;
-    Params.nbs_dir = 'scdadscv';
-    NewParams.nbs_dir = 'ascnadoicvnd';
-    NewParams.a = 10;
-
-
-end
